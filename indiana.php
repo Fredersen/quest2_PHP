6f4964f04ccaed55e8a50a8bcbc9c0215432b9f2
Naïm Jhuboo <30967014+Fredersen@users.noreply.github.com>
2021-09-26 23:54:18 +0200

    Add files via upload

---
+++ indiana.php
@@ -0,0 +1,11 @@
+<?php
+
+$originalTitle = 'Indiana Jones and the Last Crusade';
+$hasBeenSeen = true;
+$releaseDate = 1989;
+$imdbRating = 8.2;
+
+echo $originalTitle;
+echo $hasBeenSeen;
+echo $releaseDate;
+echo $imdbRating;
